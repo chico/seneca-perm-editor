6b393fa437029d76aa7f99dfe4699d14521b2e9c
Chico Charlesworth <chico.charlesworth@googlemail.com>
2014-04-15 14:45:27 +0100

    Updated UI, added saving roles logic

--- web/js/perm-editor-ng.js
+++ web/js/perm-editor-ng.js
@@ -23,10 +23,36 @@
 
   senecaPermEditorModule.controller("senecaPermEditorCtrl", ["$scope", "$rootScope", "$http", function($scope, $rootScope, $http) {
 
+    $scope.msg = '';
+
     $http({method: 'GET', url: prefix + '/rest/sys_user?admin=1', cache: false}).success(function(data, status) {
       $scope.users = data.list;
+      $scope.roles = [];
+      for(var i = 0; i < $scope.users.length; i++) {
+        $scope.roles[$scope.users[i].email] = ($scope.users[i].perm) ? $scope.users[i].perm.roles.join(',') : '';
+      }
       $scope.show_main = true;
     });
+
+    $scope.save = function() {
+      for(var i = 0; i < $scope.users.length; i++) {
+
+        var roles = $scope.roles[$scope.users[i].email].split(',');
+        for(var j = 0; j < roles.length; j++) {
+          roles[j] = roles[j].trim();
+        }
+
+        $scope.users[i].perm = {roles: roles};
+
+        var idpart = '/' + $scope.users[i].id;
+        $http({method: 'POST', url: prefix + '/rest/sys_user' +  idpart, data:$scope.users[i], cache: false}).success(function(data, status) {
+          if (status != 200) {
+            $scope.msg = "Save failed";
+          }
+        });
+
+      }
+    }
 
   }]);
 
